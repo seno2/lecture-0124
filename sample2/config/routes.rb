3df7e541e8f7459c60287fe5ca49a20efb15f47d
seno2 <kd1257172@st.kobedenshi.ac.jp>
2018-01-24 01:56:26 +0000

    01/24 Railsサンプル

--- sample2/config/routes.rb
+++ sample2/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+   root "posts#index"  # この行を追加
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
